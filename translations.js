92c66ca4e499aa259058dabd2b459f56ccd53d88
floyd666-lab <floyd666cs@gmail.com>
2026-01-29 19:31:49 +0500

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,1 +1,53 @@
+const translations = {
+  ru: {
+    schedule1: "18:00 — Сбор гостей",
+    schedule2: "19:00 — Банкет",
+    schedule3: "20:00 — Milan Band",
+    schedule4: "21:00 — Первый танец",
+    schedule5: "22:00 — Дискотека",
+    schedule6: "23:00 — Завершение",
+
+    name_placeholder: "Ваше имя",
+    music_placeholder: "Трек / Исполнитель / Жанр",
+
+    days: "Дней",
+    hours: "Часов",
+    minutes: "Минут",
+    seconds: "Секунд"
+  },
+
+  uz: {
+    schedule1: "18:00 — Mehmonlar",
+    schedule2: "19:00 — Banket",
+    schedule3: "20:00 — Milan Band",
+    schedule4: "21:00 — Birinchi raqs",
+    schedule5: "22:00 — Diskoteka",
+    schedule6: "23:00 — Yakun",
+
+    name_placeholder: "Ismingiz",
+    music_placeholder: "Qo‘shiq / Ijrochi / Janr",
+
+    days: "Kun",
+    hours: "Soat",
+    minutes: "Daqiqa",
+    seconds: "Soniya"
+  },
+
+  en: {
+    schedule1: "6:00 PM — Guests",
+    schedule2: "7:00 PM — Banquet",
+    schedule3: "8:00 PM — Milan Band",
+    schedule4: "9:00 PM — First Dance",
+    schedule5: "10:00 PM — Party",
+    schedule6: "11:00 PM — Closing",
+
+    name_placeholder: "Your name",
+    music_placeholder: "Track / Artist / Genre",
+
+    days: "Days",
+    hours: "Hours",
+    minutes: "Minutes",
+    seconds: "Seconds"
+  }
+};
 
